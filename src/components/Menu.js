eaf99a9daea56c8182def289d52f577aac2e5f42
RanaMRameez <mrameezrana99@gmail.com>
2022-07-07 14:23:33 +0500

    Added missing required files

---
+++ src/components/Menu.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MenuCard from "./MenuCard";
+
+const Menu = () => {
+  return (
+    <>
+      <MenuCard />
+      <MenuCard />
+      <MenuCard />
+    </>
+  );
+};
+
+export default Menu;
---
+++ src/components/MenuCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MenuCard = () => {
+  return (
+    <>
+      <div>Hi</div>
+    </>
+  );
+};
+
+export default MenuCard;
